870551092e797a6c804f4f816230f536a2d6621a
IbrahimAbotteen <hemaabotteen@gmail.com>
2020-08-27 14:45:02 -0700

    edit button

--- movies/src/components/Movie.jsx
+++ movies/src/components/Movie.jsx
@@ -4,7 +4,10 @@ const Movie=(props)=>{
         <div className='movie'>
         <h3>{props.movie.title}</h3>
         <h4>{props.movie.genre}</h4>
-        <p>{props.movie.description}</p>  
+        <p>{props.movie.description}</p>
+        {props.auth 
+        ? <span className="edit" onClick={() => props.setEditing(props.movie.id)}>Edit</span> 
+        : ''} 
         </div>
     )
 }
