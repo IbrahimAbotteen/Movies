afe7ba14056dfca3219898f0353e2ae7378c0deb
David Anderson <d.c.anderson@live.com>
2020-08-27 14:46:38 -0400

    Movie class and getAll method in place. Now to hook it up.

---
+++ models/Movie.js
@@ -0,0 +1,23 @@
+// Takes our database to feed into the class.
+const db = require('../db/config');
+
+// Descriptors for a movie in the DB
+class Movie {
+    constructor({ id, title, description, genre, user_id }) {
+        this.id = id || null;
+        this.title = title;
+        this.description = description;
+        this.genre = genre;
+        this.user_id = user_id;
+    }
+
+    static getAll() {
+        return db
+        .manyOrNone('SELECT * FROM movies WHERE id = $1', [id])
+        .then((movie) => {
+            if (movie) return new this(movie);
+            throw new Error(`Movie ${id} not found`);
+        });
+    }
+
+}
